845f4b01d20d5f870fe644aa7c1e60b93bc11388
Gabriel Duque <gabriel@zuh0.com>
2021-02-01 14:03:54 +0100

    users: refactor user creation
    Signed-off-by: Gabriel Duque <gabriel@zuh0.com>

--- pokedex/users.nix
+++ pokedex/users.nix
@@ -1,25 +1,29 @@
 { config, ... }:
 
-with config.sadm; {
+with config.sadm;
+let
+  makeUser = { shadowHash, extraConfig ? { } }:
+    {
+      hashedPassword = shadowHash;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFFpXDeTghddwtih0bTsx5sIIHcq3ZgGwfsU6EHtGX5i"
+      ];
+    } // extraConfig;
+in {
   users = {
     mutableUsers = false;
     users = {
-      root = {
-        hashedPassword = secrets.rootHashedPassword;
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFFpXDeTghddwtih0bTsx5sIIHcq3ZgGwfsU6EHtGX5i"
-        ];
-      };
+      root = (makeUser { shadowHash = secrets.rootHashedPassword; });
 
-      zuh0 = {
-        isNormalUser = true;
-        description = "Gabriel Duque";
-        extraGroups = [ "wheel" ];
-        hashedPassword = secrets.userHashedPassword;
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFFpXDeTghddwtih0bTsx5sIIHcq3ZgGwfsU6EHtGX5i"
-        ];
-      };
+      zuh0 = (makeUser {
+        shadowHash = secrets.userHashedPassword;
+        extraConfig = {
+          isNormalUser = true;
+          description = "Gabriel Duque";
+          extraGroups = [ "wheel" ];
+        };
+      });
+
     };
   };
 }
